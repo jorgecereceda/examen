7ce4c19b11d3d4953e00a08e823c475899ad4441
jorge <jorgecerecedatrabajo@gmail.com>
2026-05-07 13:25:59 +0200

    corrigiendo errores

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 /*fecha*/
-const meses=["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+const meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
 const dias = ["domingo", "lunes", "martes", "miercoles", "jueves", "viernes", "sabado"];
 
 const salida = document.getElementById("date");
 
 const date = new Date();
-const day=date.getDate();
-const weekday=date.getDay();
-const month=date.getMonth();
+const day = date.getDate();
+const weekday = date.getDay();
+const month = date.getMonth();
 
-const fecha = document.getElementById(id="date");
-
-salida.innerHTML=dias[weekday] + " " + day + " de " + meses[month];
+salida.innerHTML = `${dias[weekday]}, ${day} de ${meses[month]}`;
 
 
 
@@ -19,25 +17,26 @@ salida.innerHTML=dias[weekday] + " " + day + " de " + meses[month];
 const aceptar = document.getElementById("aceptar");
 const enviar = document.getElementById("enviar");
 
-const favcolor=document.getElementById("favcolor");
-const valoracion=document.getElementById("valoracion");
-const sugerencia=document.getElementById("sugerencia");
+const favcolor = document.getElementById("favcolor");
+const valoracion = document.getElementById("valoracion");
+const sugerencia = document.getElementById("sugerencia");
 const radios = document.querySelectorAll('input[name="tamano"]');
-let radioseleccion="pequeño";
+
+let radioseleccion = "pequeño";
 
 radios.forEach(radio => {
     radio.addEventListener('change', (e) => {
-      radioseleccion = e.target.value;
+        radioseleccion = e.target.value;
     });
-  });
+});
 
-aceptar.addEventListener("change", ()=>{
+aceptar.addEventListener("change", () => {
     enviar.disabled = !aceptar.checked;
 });
 
-enviar.addEventListener("click", function(event){
-  event.preventDefault();
-  alert(`tamaño: ${radioseleccion}
+enviar.addEventListener("click", function (event) {
+    event.preventDefault();
+    alert(`tamaño: ${radioseleccion}
 color: ${favcolor.value}
 valoración: ${valoracion.value}
 sugerencias: ${sugerencia.value}`);
@@ -45,51 +44,51 @@ sugerencias: ${sugerencia.value}`);
 
 
 /*cambio imagen*/
-const imagen=document.getElementById("imagen");
+const imagen = document.getElementById("imagen");
 
-let imagen1="img/Trabajando1.webp";
-let imagen2="img/Trabajando2.jpg";
+let imagen1 = "img/Trabajando1.webp";
+let imagen2 = "img/Trabajando2.jpg";
 
-imagen.addEventListener("click", ()=>{
-    let imagenActual=imagen.getAttribute("src");
-     if(imagenActual==imagen1)  {
+imagen.addEventListener("click", () => {
+    let imagenActual = imagen.getAttribute("src");
+    if (imagenActual == imagen1) {
 
-                                imagen.setAttribute("src", imagen2);
+        imagen.setAttribute("src", imagen2);
 
-                            } else {  
-                                imagen.setAttribute("src", imagen1);
+    } else {
+        imagen.setAttribute("src", imagen1);
 
-                            }
+    }
 });
 
 
 /*login*/
-const validname="user1";
-const validpasword="123";
+const validname = "user1";
+const validpasword = "123";
 
 const popup = document.getElementById("modal");
 const loginbutton = document.getElementById("loginbutton");
 const closebutton = document.getElementById("closebutton");
 
-loginbutton.addEventListener("click", ()=>{
+loginbutton.addEventListener("click", () => {
     modal.style.display = "block";
 
 });
-closebutton.addEventListener("click", ()=>{
+closebutton.addEventListener("click", () => {
     modal.style.display = "none";
 
 });
 
-const username =document.getElementById("username");
-const password=document.getElementById("pasword");
-const login=document.getElementById("login");
+const username = document.getElementById("username");
+const password = document.getElementById("pasword");
+const login = document.getElementById("login");
 
 login.addEventListener("click", function (event) {
-      event.preventDefault();
-    if (username.value==validname && password.value==validpasword){
+    event.preventDefault();
+    if (username.value == validname && password.value == validpasword) {
         alert("usuario admitido");
         localStorage.setItem('login', 'true');
     } else {
-        alert ("usuario no admitido");
+        alert("usuario no admitido");
     }
 });
